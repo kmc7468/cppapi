57337d9a4263430f70ecb55104d352b04fa6eb46
static <kmc7468@naver.com>
2017-09-30 13:29:53 +0900

    config.hh 추가

---
+++ include/cppapi/config.hh
@@ -0,0 +1,28 @@
+#ifndef CPPAPI_HEADER_CONFIG_HH
+#define CPPAPI_HEADER_CONFIG_HH
+
+#if defined(CPPAPI_MODULE) && defined(CPPAPI_DYNAMIC) && defined(_MSC_VER)
+#	error To compile module on MSVC, you must use static linking.
+#endif
+
+#if defined(CPPAPI) && defined(CPPAPI_MODULE)
+#	define CPPAPI_EXPORT export
+#	define CPPAPI_DLLEXPORT
+#elif defined(CPPAPI) && defined(CPPAPI_DYNAMIC) && defined(_MSC_VER)
+#	define CPPAPI_EXPORT
+#	define CPPAPI_DLLEXPORT __declspec(dllexport)
+#elif !defined(CPPAPI) && defined(CPPAPI_DYNAMIC) && defined(_MSC_VER)
+#	define CPPAPI_EXPORT
+#	define CPPAPI_DLLEXPORT __declspec(dllimport)
+#else
+#	define CPPAPI_EXPORT
+#	define CPPAPI_DLLEXPORT
+#endif
+
+#define CPPAPI_CLASS						\
+CPPAPI_EXPORT class CPPAPI_DLLEXPORT
+
+#define CPPAPI_STRUCT						\
+CPPAPI_EXPORT struct CPPAPI_DLLEXPORT
+
+#endif
