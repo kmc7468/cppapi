f65bef4d32b3ae93991d099f55ef6f12b4b812e0
static <kmc7468@naver.com>
2017-10-01 13:14:56 +0900

    컴파일 제한 추가

--- src/cppapi/cppapi.cc
+++ src/cppapi/cppapi.cc
@@ -4,3 +4,10 @@ module cppapi;
 export import cppapi.project;
 export import cppapi.source;
 #endif
+
+#include <climits>
+#include <cstddef>
+
+static_assert(CHAR_BIT == 8, "Only systems with 8 bits per byte are supported.");
+static_assert(sizeof(char) == 1, "Only systems with char's a size of 1 byte are supported.");
+static_assert(sizeof(std::size_t) >= 4, "Only compilers with std::size_t's a size of 4 bytes are supported.");
