43972ef42c6b94fc50ba906a55b63a95d9aa32e5
static <kmc7468@naver.com>
2017-10-07 18:13:15 +0900

    project::remove_source 메서드 추가

--- src/cppapi/cppapi.cc
+++ src/cppapi/cppapi.cc
@@ -7,7 +7,9 @@ export import cppapi.source;
 
 #include <climits>
 #include <cstddef>
+#include <type_traits>
 
 static_assert(CHAR_BIT == 8, "Only systems with 8 bits per byte are supported.");
 static_assert(sizeof(char) == 1, "Only systems with char's a size of 1 byte are supported.");
 static_assert(sizeof(std::size_t) >= 4, "Only compilers with std::size_t's a size of 4 bytes are supported.");
+static_assert(std::is_unsigned<std::size_t>::value, "Only compilers with std::size_t is unsigned are supported.");
